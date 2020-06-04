b20d2115d99753f1c02cf9d4793c2494ac0f5ece
vinicius-dev-selletiva <vinicius26092000@gmail.com>
2020-06-04 11:39:53 -0300

    correção success

--- knexfile.ts
+++ knexfile.ts
@@ -16,4 +16,18 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+    useNullAsDefault: true
+  }
+
 };
